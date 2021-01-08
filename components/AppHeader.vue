c3fa2689a57e061453d360cf644590f71ad9668b
naisuu <julius.cathalina@gmail.com>
2021-01-08 06:55:18 +0100

    Add start of navbar and first section

---
+++ components/AppHeader.vue
@@ -0,0 +1,48 @@
+<template>
+  <header class="header">
+    <nuxt-link to="/">
+      <strong class="brand">Kenepa Tech</strong>
+    </nuxt-link>
+    <nav>
+      <ul>
+        <li>
+          <nuxt-link to="/">Home</nuxt-link>
+        </li>
+        <li>
+          <nuxt-link to="/about">About</nuxt-link>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'AppHeader',
+}
+</script>
+
+<style>
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 1rem;
+  padding-bottom: 1rem;
+}
+.header .brand {
+  font-size: 3rem;
+  color: #278d2b;
+}
+.header ul {
+  display: flex;
+}
+.header a {
+  display: inline-block;
+  background: #333;
+  color: #fff;
+  padding: 0.3rem 1rem;
+  margin-right: 0.5rem;
+  border-radius: 5%;
+}
+</style>
---
+++ components/SectionHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <section>
+    <span>
+      
+    </span>
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
