98281e373c1b5c378f8ae450111ba13d4190953b
naisuu <julius.cathalina@gmail.com>
2021-01-12 05:31:12 +0100

    add placeholder pages

---
+++ pages/contact.vue
@@ -0,0 +1,5 @@
+<template>
+<div class="container">
+    <span class="emoji">🚧<h1 class="title">CONTACT US</h1>🚧</span>
+</div>
+</template>
---
+++ pages/projects.vue
@@ -0,0 +1,5 @@
+<template>
+<div class="container">
+    <span class="emoji">🚧<h1 class="title"> PROJECTS</h1>🚧</span>
+</div>
+</template>
