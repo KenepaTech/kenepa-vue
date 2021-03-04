ce22073debda152035737adc6b2b5c4d4f95e692
Neogarash <m3mccloud@gmail.com>
2021-03-04 17:11:03 +0100

    file changes for the vue3

---
+++ src/components/SideBarMobile.vue
@@ -0,0 +1,95 @@
+<template>
+<div class="sidebar">
+  <ul>
+  <li>
+    <a href="https://twitter.com/KenepaTech" target="_blank" title="Go to Kenepa Tech's Twitter page">
+      <i><fa-icon :icon="['fab','twitter']" /></i>
+    </a>
+  </li>
+  <li>
+    <a href="https://www.instagram.com/kenepatech/" target="_blank" title="Go to Kenepa Tech's Instagram page">
+     <i><fa-icon :icon="['fab','instagram']" /></i>
+    </a>
+    </li>
+  <li>
+    <a href="https://www.linkedin.com/in/julius-cathalina/" target="_blank" title="Go to Kenepa Tech's Linkedin page">
+      <i> <fa-icon :icon="['fab','linkedin-in']" /></i>
+    </a>
+    
+  </li>
+  <li>
+    <a href="https://github.com/KenepaTech" target="_blank" title="Go to Kenepa Tech's Github page">
+      <i><fa-icon :icon="['fab','github-alt']" /></i>
+    </a> 
+  </li>
+</ul>
+</div>
+</template>
+
+<style scoped>
+
+.sidebar{
+  position: absolute;
+  width: 100%;
+  margin: auto;
+  display: flex;
+  justify-content: center;
+
+}
+
+ul {
+  margin: 0;
+  padding: 0;
+  display: flex;
+  position: inherit;
+}
+li{
+  padding: 0 1rem;
+}
+
+ul li {
+  list-style: none;
+  margin: 15px 0;
+}
+
+ul li a {
+  position: relative; 
+  display: block;
+  width: 30px;
+  height: 30px;
+  text-align: center;
+  line-height: 30px;
+  background-color:var(--kenepa-orange-alt);
+  border-radius: 50%;
+  font-size: 15px;
+  color: var(--bg-color);
+  transition: .5s;
+}
+
+ul li a::before {
+  content: '';
+  position: absolute;
+  display:block;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  border-radius: 50%;
+  background-color: var(--kenepa-green-alt);
+  transition: .5s;
+  transform: scale(.9);
+  z-index: -1;
+}
+
+ul li a:hover::before {
+  transform: scale(1.1);
+  box-shadow: 0 0 15px var(--kenepa-green-alt);
+}
+
+ul li a:hover {
+  color: var(--kenepa-green-alt);
+  box-shadow: 0 0 5px var(--kenepa-green-alt);
+  text-shadow: 0 0 5px var(--kenepa-green-alt);
+  
+} 
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+    <Logo />
+    <h1 class="title">Kenepa Tech</h1>
+    <WelcomeBanner />
+    <GridLinks />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
