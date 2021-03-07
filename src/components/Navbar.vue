8369105f03e24be1a5019749550457e3321f7f87
rocheandley <rocheandley@gmail.com>
2021-03-07 23:08:29 +0100

    added navigation change on scroll

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="bg-gray-800">
+  <nav  :class="{change_color: scrollPosition > 50}" class="bg-gray-800">
     <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
       <div class="relative flex items-center justify-between h-16">
         <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
@@ -50,15 +50,15 @@
           class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start"
         >
           <div class="flex-shrink-0 flex items-center">
-            <router-link to="/"><img
+            <router-link to="/"><img :class="{change_photo: scrollPosition > 50}"
               class="block lg:hidden h-8 w-auto"
               src="/icon.png"
               alt="Kenepa"
             /></router-link>
-            <router-link to="/" class="bg-gray-900 text-white">
+            <router-link to="/" >
             <img
               class="hidden lg:block h-8 w-auto"
-              src="/icon-wft.png"
+              src="/icon.png"
               alt="Kenepa"
             /></router-link>
           </div>
@@ -77,80 +77,7 @@
         <div
           class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0"
         >
-        <!-- Heroicon name: outline/bell -->
-          <!-- <button
-            class="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-          >
-            <span class="sr-only">View notifications</span>
-            
-            <svg
-              class="h-6 w-6"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              aria-hidden="true"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-              />
-            </svg>
-          </button> -->
 
-          <!-- Profile dropdown -->
-          <!-- <div class="ml-3 relative">
-            <div>
-              <button
-                type="button"
-                @click="userMenuIsOpen = !userMenuIsOpen"
-                class="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-                id="user-menu"
-                aria-haspopup="true"
-              >
-                <span class="sr-only">Open user menu</span>
-                <img
-                  class="h-8 w-8 rounded-full"
-                  src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                  alt=""
-                />
-              </button>
-              <transition
-                enter-active-class="transition ease-out duration-100"
-                enter-from-class="transform opacity-0 scale-95"
-                enter-to-class="transform opacity-100 scale-100"
-                leave-active-class="transition ease-in duration-75"
-                leave-from-class="transform opacity-100 scale-100"
-                leave-to-class="transform opacity-0 scale-95"
-              >
-                <div
-                  v-show="userMenuIsOpen"
-                  class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5"
-                >
-                  <a
-                    href="#"
-                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                    role="menuitem"
-                    >Your Profile</a
-                  >
-                  <a
-                    href="#"
-                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                    role="menuitem"
-                    >Settings</a
-                  >
-                  <a
-                    href="#"
-                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                    role="menuitem"
-                    >Sign out</a
-                  >
-                </div>
-              </transition>
-            </div>
-          </div> -->
         </div>
       </div>
     </div>
@@ -169,12 +96,55 @@
   </nav>
 </template>
 
+<style scoped>
+
+nav {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+}
+
+.change_color {
+  background-color:#141e24;
+
+}
+
+.change_color img{
+  background-color:#141e24;
+  content:url("kenepa_logo_w.png");
+  color:red;
+}
+
+
+.change_photo img{
+  /*content:url("kenepa_logo_w.png")*/
+
+}
+
+</style>
+
 <script>
 export default {
   name: 'Navbar',
   data: () => ({
     menuIsOpen: false,
     userMenuIsOpen: false,
+    scrollPosition: null
+
+
   }),
+
+  mounted() {
+    window.addEventListener('scroll', this.updateScroll);
+  },
+
+
+  methods: {
+    updateScroll() {
+      this.scrollPosition = window.scrollY
+    }
+  }
+
 };
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,8 +2,65 @@
   <div class="container">
     <!-- <Logo /> -->
     <h1 class="title">Kenepa Tech This is the home page</h1>
+
     <WelcomeBanner />
     <!-- <GridLinks /> -->
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dignissimos dolor dolores ex exercitationem fugiat labore nobis porro sint voluptates!
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dignissimos dolor dolores ex exercitationem fugiat labore nobis porro sint voluptates!
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dignissimos dolor dolores ex exercitationem fugiat labore nobis porro sint voluptates!
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dignissimos dolor dolores ex exercitationem fugiat labore nobis porro sint voluptates!
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dignissimos dolor dolores ex exercitationem fugiat labore nobis porro sint voluptates!
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dignissimos dolor dolores ex exercitationem fugiat labore nobis porro sint voluptates!
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dignissimos dolor dolores ex exercitationem fugiat labore nobis porro sint voluptates!
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dignissimos dolor dolores ex exercitationem fugiat labore nobis porro sint voluptates!
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dignissimos dolor dolores ex exercitationem fugiat labore nobis porro sint voluptates!
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dignissimos dolor dolores ex exercitationem fugiat labore nobis porro sint voluptates!
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dignissimos dolor dolores ex exercitationem fugiat labore nobis porro sint voluptates!
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dignissimos dolor dolores ex exercitationem fugiat labore nobis porro sint voluptates!
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dignissimos dolor dolores ex exercitationem fugiat labore nobis porro sint voluptates!
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dignissimos dolor dolores ex exercitationem fugiat labore nobis porro sint voluptates!
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dignissimos dolor dolores ex exercitationem fugiat labore nobis porro sint voluptates!
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dignissimos dolor dolores ex exercitationem fugiat labore nobis porro sint voluptates!
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dignissimos dolor dolores ex exercitationem fugiat labore nobis porro sint voluptates!
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dignissimos dolor dolores ex exercitationem fugiat labore nobis porro sint voluptates!
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dignissimos dolor dolores ex exercitationem fugiat labore nobis porro sint voluptates!
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dignissimos dolor dolores ex exercitationem fugiat labore nobis porro sint voluptates!
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dignissimos dolor dolores ex exercitationem fugiat labore nobis porro sint voluptates!
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dignissimos dolor dolores ex exercitationem fugiat labore nobis porro sint voluptates!
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dignissimos dolor dolores ex exercitationem fugiat labore nobis porro sint voluptates!
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dignissimos dolor dolores ex exercitationem fugiat labore nobis porro sint voluptates!
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dignissimos dolor dolores ex exercitationem fugiat labore nobis porro sint voluptates!
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dignissimos dolor dolores ex exercitationem fugiat labore nobis porro sint voluptates!
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dignissimos dolor dolores ex exercitationem fugiat labore nobis porro sint voluptates!
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dignissimos dolor dolores ex exercitationem fugiat labore nobis porro sint voluptates!
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dignissimos dolor dolores ex exercitationem fugiat labore nobis porro sint voluptates!
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dignissimos dolor dolores ex exercitationem fugiat labore nobis porro sint voluptates!
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dignissimos dolor dolores ex exercitationem fugiat labore nobis porro sint voluptates!
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dignissimos dolor dolores ex exercitationem fugiat labore nobis porro sint voluptates!
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dignissimos dolor dolores ex exercitationem fugiat labore nobis porro sint voluptates!
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dignissimos dolor dolores ex exercitationem fugiat labore nobis porro sint voluptates!
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dignissimos dolor dolores ex exercitationem fugiat labore nobis porro sint voluptates!
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dignissimos dolor dolores ex exercitationem fugiat labore nobis porro sint voluptates!
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dignissimos dolor dolores ex exercitationem fugiat labore nobis porro sint voluptates!
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dignissimos dolor dolores ex exercitationem fugiat labore nobis porro sint voluptates!
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dignissimos dolor dolores ex exercitationem fugiat labore nobis porro sint voluptates!
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dignissimos dolor dolores ex exercitationem fugiat labore nobis porro sint voluptates!
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dignissimos dolor dolores ex exercitationem fugiat labore nobis porro sint voluptates!
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dignissimos dolor dolores ex exercitationem fugiat labore nobis porro sint voluptates!
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dignissimos dolor dolores ex exercitationem fugiat labore nobis porro sint voluptates!
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dignissimos dolor dolores ex exercitationem fugiat labore nobis porro sint voluptates!
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dignissimos dolor dolores ex exercitationem fugiat labore nobis porro sint voluptates!
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dignissimos dolor dolores ex exercitationem fugiat labore nobis porro sint voluptates!
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dignissimos dolor dolores ex exercitationem fugiat labore nobis porro sint voluptates!
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dignissimos dolor dolores ex exercitationem fugiat labore nobis porro sint voluptates!
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dignissimos dolor dolores ex exercitationem fugiat labore nobis porro sint voluptates!
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dignissimos dolor dolores ex exercitationem fugiat labore nobis porro sint voluptates!
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dignissimos dolor dolores ex exercitationem fugiat labore nobis porro sint voluptates!
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dignissimos dolor dolores ex exercitationem fugiat labore nobis porro sint voluptates!
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dignissimos dolor dolores ex exercitationem fugiat labore nobis porro sint voluptates!
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dignissimos dolor dolores ex exercitationem fugiat labore nobis porro sint voluptates!
+    </p>
   </div>
 </template>
 
