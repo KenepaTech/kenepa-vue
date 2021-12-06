37051b204e78c74997c2842643ebee4224642684
naisuu <julius.cathalina@gmail.com>
2021-12-06 17:49:45 +0100

    Add vue router plugin

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import Vue from "vue";
 import App from "./App.vue";
+
 // import { Table, Input } from 'buefy'; // <-- Example of how to pull in individual buefy components
 import "buefy/dist/buefy.css";
+
+import router from './router'
 
 Vue.config.productionTip = false;
 
 new Vue({
-  render: (h) => h(App),
+  router,
+  render: (h) => h(App)
 }).$mount("#app");
