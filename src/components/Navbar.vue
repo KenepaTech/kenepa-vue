d18710eebdf9455956bfeddfeccbf238d1addad6
naisuu <julius.cathalina@gmail.com>
2021-12-06 21:03:50 +0100

    Add Navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+  <b-navbar>
+    <template #brand>
+      <b-navbar-item tag="router-link" :to="{ path: '/' }">
+        <img
+          src="@/assets/logo.png"
+          alt="Kenepa Logo"
+        />
+      </b-navbar-item>
+    </template>
+    <template #start>
+      <b-navbar-item tag="router-link" :to="{ path: '/' }"> Home </b-navbar-item>
+      <b-navbar-item tag="router-link" :to="{ path: '/about' }"> About </b-navbar-item>
+      <b-navbar-item tag="router-link" :to="{ path: '/contact' }"> Contact </b-navbar-item>
+    </template>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -2,11 +2,14 @@ import Vue from "vue";
 import App from "./App.vue";
 
 // import { Table, Input } from 'buefy'; // <-- Example of how to pull in individual buefy components
+import Buefy from "buefy";
 import "buefy/dist/buefy.css";
 
 import router from "./router";
 
 Vue.config.productionTip = false;
+
+Vue.use(Buefy);
 
 new Vue({
   router,
