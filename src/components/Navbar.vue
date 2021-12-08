2ac7486a58b3615cb9cab99e232aaebbd9a193b8
naisuu <julius.cathalina@gmail.com>
2021-12-08 19:56:49 +0100

    Remove buefy

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,19 +1,5 @@
 <template>
-  <b-navbar>
-    <template #brand>
-      <b-navbar-item tag="router-link" :to="{ path: '/' }">
-        <img
-          src="@/assets/Kenepa_Logo_V3_WT.png"
-          alt="Kenepa Logo"
-        />
-      </b-navbar-item>
-    </template>
-    <template #start>
-      <b-navbar-item tag="router-link" :to="{ path: '/' }"> Home </b-navbar-item>
-      <b-navbar-item tag="router-link" :to="{ path: '/about' }"> About </b-navbar-item>
-      <b-navbar-item tag="router-link" :to="{ path: '/contact' }"> Contact </b-navbar-item>
-    </template>
-  </b-navbar>
+  <h1>Placeholder nav</h1>
 </template>
 
 <script>
@@ -23,11 +9,5 @@ export default {
 </script>
 
 <style scoped>
-  .navbar {
-    background-color: var(--main-bg-color);
-  }
 
-  .navbar-item {
-    color: white;
-  }
 </style>
